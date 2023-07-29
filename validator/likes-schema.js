48828c4bae92a789e721557518644a83b436d4df
GraemeAngusJones <48164351+graemeajones@users.noreply.github.com>
2023-07-29 11:34:29 +0100

    Update likes-schema.js

--- validator/likes-schema.js
+++ validator/likes-schema.js
@@ -7,8 +7,8 @@ schema.mutableFields = ['LikerID', 'LikeeID', 'LikeAffinityID'];
 schema.record = joi
   .object({
     LikerID: joi.number().integer().min(1).allow(null),
-    LikeeID: joi.number().integer().min(1).allow(null),
-    LikeAffinityID: joi.number().integer().min(1).allow(null),
+    LikeeID: joi.number().integer().min(1),
+    LikeAffinityID: joi.number().integer().min(1),
   })
   .required()
   .unknown(true);
