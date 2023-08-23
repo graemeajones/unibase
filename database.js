9a4362fbebbbcf8ae70b186d466a7a47858fa88a
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2023-08-23 12:34:20 +0100

    Using pools instead of connection (#33)

--- database.js
+++ database.js
@@ -1,25 +1,37 @@
-// Imports ---------------------------------------
-import mysql from 'mysql2/promise';
-
-// Database Connection ---------------------------
+import mysql from "mysql2/promise";
 
 const dbConfig = {
-  database: process.env.DB_NAME || 'UniBase',
+  database: process.env.DB_NAME || "UniBase",
   port: process.env.DB_PORT || 3306,
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PSWD || '',
-  namedPlaceholders: true
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PSWD || "",
+  namedPlaceholders: true,
 };
 
-let database = null;
+const createDbConnection = async () => {
+  try {
+    return await mysql.createConnection(dbConfig);
+  } catch (error) {
+    console.log("Error creating database connection: " + error.message);
+    process.exit();
+  }
+};
 
-try {
-  database = await mysql.createConnection(dbConfig);
-}
-catch (error) {
-  console.log('Error creating database connection: ' + error.message);
-  process.exit();
-}
+const createDbConnectionPool = () => {
+  try {
+    return mysql.createPool({
+      ...dbConfig,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+      enableKeepAlive: true,
+      keepAliveInitialDelay: 0,
+    });
+  } catch (error) {
+    console.log("Error creating database connection: " + error.message);
+    process.exit();
+  }
+};
 
-export default database;
+export default createDbConnectionPool();
