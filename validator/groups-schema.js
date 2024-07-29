06cbe5627a7da5a2bb5995843f09e3a2aa760751
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2024-07-29 15:32:46 +0100

    Fixed

--- validator/groups-schema.js
+++ validator/groups-schema.js
@@ -2,13 +2,13 @@ import joi from 'joi';
 
 const schema = {};
 
-schema.mutableFields = ['GroupName', 'GroupProjectID'];
+schema.mutableFields = ['GroupName', 'GroupAssessmentID'];
 
 schema.record = joi
   .object({
     GroupID: joi.number().integer().min(1).allow(null),
     GroupName: joi.string().min(8),
-    GroupProjectID: joi.number().integer().min(1).allow(null),
+    GroupAssessmentID: joi.number().integer().min(1).allow(null),
   })
   .required()
   .unknown(true);
