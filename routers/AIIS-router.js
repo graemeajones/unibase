e5d3c3ca130fa191123b5aa6b733d75084579abe
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2024-07-31 23:33:33 +0100

    Added jobtitles endpoints

--- routers/AIIS-router.js
+++ routers/AIIS-router.js
@@ -1,6 +1,7 @@
 // Imports ---------------------------------------
 import { Router } from 'express';
 import experienceRouter from './AIIS/experience-router.js';
+import jobtitlesRouter from './AIIS/jobtitles-router.js';
 import technologiesRouter from './AIIS/technologies-router.js';
 
 // Endpoints -------------------------------------
@@ -8,6 +9,7 @@ import technologiesRouter from './AIIS/technologies-router.js';
 const router = new Router({ mergeParams: true });
 
 router.use('/experience', experienceRouter);
+router.use('/jobtitles', jobtitlesRouter);
 router.use('/technologies', technologiesRouter);
 
 export default router;
