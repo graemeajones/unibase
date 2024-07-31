d06a1613bd29d92cdc5d9111e07448afc17ce0b7
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2024-07-31 23:03:55 +0100

    Adding prompts endpoints (#47)
    * Add AIIS technologies endpoint
    
    * Experience endpoint added

--- routers/AIIS-router.js
+++ routers/AIIS-router.js
@@ -1,11 +1,14 @@
 // Imports ---------------------------------------
 import { Router } from 'express';
+
+import experienceRouter from './AIIS/experience-router.js';
 import technologiesRouter from './AIIS/technologies-router.js';
 
 // Endpoints -------------------------------------
 
 const router = new Router({ mergeParams: true });
 
+router.use('/experience', experienceRouter);
 router.use('/technologies', technologiesRouter);
 
 export default router;
