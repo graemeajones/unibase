cf0d20c3e9191b5af61b4f3fa7c70e835310cffe
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2025-02-25 00:41:08 +0000

    And the status endpoints! (#61)

--- projects/staysafev1/models/status-model.js
+++ projects/staysafev1/models/status-model.js
@@ -1,39 +1,28 @@
 import { parseRequestQuery, constructPreparedStatement } from '#root/models/utils.js';
 
 const model = {
-  table: 'Users',
-  idField: 'UserID',
-  mutableFields: ['UserFirstname', 'UserLastname', 'UserPhone', 'UserUsername'],
+  table: 'Status',
+  idField: 'StatusID',
+  mutableFields: ['StatusName', 'StatusOrder'],
 
   buildReadQuery: (req, variant) => {
     // Initialisations ------------------------
     // Resolve Foreign Keys -------------------
-    let table = 'Users';
+    let table = 'Status';
     let fields = [model.idField, ...model.mutableFields];
 
     // Construct prepared statement -----------
     let where = null;
     let parameters = {};
     switch (variant) {
-      case 'contacts':
-        fields = [
-          ...fields,
-          'ContactID AS UserContactID',
-          'ContactLabel AS UserContactLabel',
-          'ContactDatecreated AS UserContactDatecreated',
-        ];
-        table = `Contacts INNER JOIN ${table} ON ContactContactID=Users.UserID`;
-        where = 'ContactUserID=:ID';
-        parameters = { ID: parseInt(req.params.id) };
-        break;
       case 'primary':
-        where = 'UserID=:ID';
+        where = 'StatusID=:ID';
         parameters = { ID: parseInt(req.params.id) };
         break;
     }
 
     // Process request queries ----------------
-    const allowedQueryFields = [...model.mutableFields, 'UserContactLabel'];
+    const allowedQueryFields = [...model.mutableFields];
     const [filter, orderby] = parseRequestQuery(req, allowedQueryFields);
 
     return constructPreparedStatement(fields, table, where, parameters, filter, orderby);
---
+++ projects/staysafev1/schemas/status-schema.js
@@ -0,0 +1,18 @@
+import joi from 'joi';
+
+const schema = {};
+
+schema.mutableFields = ['StatusName', 'StatusOrder'];
+
+schema.record = joi
+  .object({
+    StatusID: joi.number().integer().min(1).allow(null),
+    StatusName: joi.string().min(4),
+    StatusOrder: joi.number().integer(),
+  })
+  .required()
+  .unknown(true);
+
+schema.conformor = {};
+
+export default schema;
