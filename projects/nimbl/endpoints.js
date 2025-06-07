6f08ab87b71ef1ac0d36d2dec3e530c5a0b6dc7f
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2025-06-08 00:42:38 +0100

    CRUDL endpoints added (#79)

--- projects/nimbl/endpoints.js
+++ projects/nimbl/endpoints.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 
 //import eventsRouter from './routers/events-router.js';
 import usersRouter from './routers/users-router.js';
+import usertypesRouter from './routers/usertypes-router.js';
 
 import API_URL from '#root/apiURL.js';
 
@@ -76,6 +77,36 @@ const listOfEndpoints = [
       },
     },
   },
+  {
+    entity: 'User types',
+    sap: '/api/usertypes',
+    services: {
+      get: [
+        {
+          endpoint: '/',
+          description: 'Returns all user types',
+          examples: [`${API_PATH}/usertypes`],
+        },
+        {
+          endpoint: '/{id}',
+          description: 'Returns the specific user type identified by the id provided',
+          example: `${API_PATH}/usertypes/1`,
+        },
+      ],
+      post: {
+        endpoint: '/',
+        description: 'Insert a new user type',
+      },
+      put: {
+        endpoint: '/{id}',
+        description: 'Update the specific user type identified by the id provided',
+      },
+      delete: {
+        endpoint: '/{id}',
+        description: 'Delete the specific user type identified by the id provided',
+      },
+    },
+  },
 ];
 
 // Endpoints -------------------------------------
@@ -84,6 +115,7 @@ const router = new Router({ mergeParams: true });
 
 // router.use('/events', eventsRouter);
 router.use('/users', usersRouter);
+router.use('/usertypes', usertypesRouter);
 
 router.get('/', (req, res) =>
   res.status(200).json({
