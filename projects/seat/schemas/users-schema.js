f30bc7d70c845f4d240494b73cee1bfa54c66166
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2025-08-07 12:27:01 +0100

    Seats/mod/add useractive column to users table (#105)
    * Seat/mod/UserActive field added to Users table
    
    * Seat/mod/Changed UserActive field to 0,1 rather than boolean

--- projects/seat/schemas/users-schema.js
+++ projects/seat/schemas/users-schema.js
@@ -24,14 +24,13 @@ schema.record = joi
     UserUsertypeID: joi.number().integer().min(1).allow(null),
     UserRoleID: joi.number().integer().min(1).allow(null),
     UserGuestofID: joi.number().integer().min(1).allow(null),
-    UserActive: joi.boolean(),
+    UserActive: joi.number().integer().valid(0, 1),
   })
   .required()
   .unknown(true);
 
 schema.conformor = {
   UserDateofbirth: (value) => (value === null ? null : new Date(value)),
-  UserActive: (value) => (value ? true : false),
 };
 
 export default schema;
